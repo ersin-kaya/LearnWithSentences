e73b572eaa8cc4b743e89b3e0355e4bd83c0e17c
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-06-20 02:47:26 +0300

    Add LanguageManager.cs

---
+++ Business/Concrete/LanguageManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+	public class LanguageManager : ILanguageService
+	{
+        ILanguageDal _languageDal;
+
+        public LanguageManager(ILanguageDal languageDal)
+        {
+            _languageDal = languageDal;
+        }
+
+        public IResult Add(Language language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(Language language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Language>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<Language> GetById(int languageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Language language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
