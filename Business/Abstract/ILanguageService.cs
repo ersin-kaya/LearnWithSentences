d1c44a36c5ed36cfdc860891e6f777bf98ab644b
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-06-20 02:44:08 +0300

    Add ILanguageService.cs

---
+++ Business/Abstract/ILanguageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+	public interface ILanguageService
+	{
+		IResult Add(Language language);
+		IResult Update(Language language);
+		IResult Delete(Language language);
+        IDataResult<List<Language>> GetAll();
+        IDataResult<Language> GetById(int languageId);
+	}
+}
+
