f2e9013a7dfa9e5a274eb0c68bf7787404967699
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-06-26 23:53:38 +0300

    Add the Folder entity's Business objects

---
+++ Business/Abstract/IFolderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+	public interface IFolderService
+    {
+        IResult Add(Folder folder);
+        IDataResult<List<Folder>> GetAll();
+        IDataResult<Folder> GetById(int folderId);
+        IDataResult<List<Folder>> GetByAccountId(int accountId);
+    }
+}
+
---
+++ Business/Concrete/FolderManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Business.Abstract;
+using Business.Constants.Messages.Abstract;
+using Core.Utilities.Business;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+	public class FolderManager : IFolderService
+	{
+        IFolderDal _folderDal;
+        IMessage _message;
+
+		public FolderManager(IFolderDal folderDal, IMessage message)
+		{
+            _folderDal = folderDal;
+            _message = message;
+		}
+
+        public IResult Add(Folder folder)
+        {
+            IResult result = BusinessRules.Run(CheckIfFolderNameExists(folder.Name));
+            if (result != null)
+            {
+                return result;
+            }
+
+            _folderDal.Add(folder);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Folder>> GetAll()
+        {
+            return new SuccessDataResult<List<Folder>>(_folderDal.GetAll());
+        }
+
+        public IDataResult<List<Folder>> GetByAccountId(int accountId)
+        {
+            return new SuccessDataResult<List<Folder>>(_folderDal.GetAll(f => f.AccountId == accountId));
+        }
+
+        public IDataResult<Folder> GetById(int folderId)
+        {
+            return new SuccessDataResult<Folder>(_folderDal.Get(f => f.Id == folderId));
+        }
+
+        private IResult CheckIfFolderNameExists(string folderName)
+        {
+            var result = _folderDal.GetAll(f => f.Name == folderName).Any();
+            if (result)
+            {
+                return new ErrorResult();
+            }
+            return new SuccessResult();
+        }
+    }
+}
+
