567b4a642889615541cf806073504b3d55782017
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-06-27 00:02:25 +0300

    Add new constant messages

--- Business/Constants/Messages/Abstract/IMessage.cs
+++ Business/Constants/Messages/Abstract/IMessage.cs
@@ -3,11 +3,17 @@ namespace Business.Constants.Messages.Abstract
 {
 	public interface IMessage
 	{
+        //Language
         string LanguagesListed { get; }
         string LanguageAlreadyExists { get; }
         string LanguageAdded { get; }
         string LanguageDeleted { get; }
         string LanguageUpdated { get; }
+
+        //Folder
+        string FoldersListed { get; }
+        string FolderAlreadyExists { get; }
+        string FolderAdded { get; }
     }
 }
 
--- Business/Constants/Messages/Concrete/MessagesInEnglish.cs
+++ Business/Constants/Messages/Concrete/MessagesInEnglish.cs
@@ -7,18 +7,30 @@ namespace Business.Constants.Messages.Concrete
     {
         public MessagesInEnglish()
         {
+            //Language
             LanguagesListed = "Languages were listed.";
             LanguageAlreadyExists = "The language already exists.";
             LanguageAdded = "The language has been successfully added.";
             LanguageDeleted = "The language has been deleted.";
             LanguageUpdated = "The language has been updated.";
+
+            //Folder
+            FoldersListed = "Folders were listed.";
+            FolderAlreadyExists = "The folder name already exists.";
+            FolderAdded = "The folder has been successfully added.";
         }
 
+        //Language
         public string LanguagesListed { get; }
         public string LanguageAlreadyExists { get; }
         public string LanguageAdded { get; }
         public string LanguageDeleted { get; }
         public string LanguageUpdated { get; }
+
+        //Folder
+        public string FoldersListed { get; }
+        public string FolderAlreadyExists { get; }
+        public string FolderAdded { get; }
     }
 }
 
