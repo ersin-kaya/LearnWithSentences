d7315631a5236fcdfc11f7f18f86583d9d32f3ea
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-06-28 00:07:02 +0300

    Add FoldersController.cs

---
+++ WebApi/Controllers/FoldersController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FoldersController : ControllerBase
+    {
+        IFolderService _folderService;
+
+        public FoldersController(IFolderService folderService)
+        {
+            _folderService = folderService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _folderService.GetAll();
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _folderService.GetById(id);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyaccountid")]
+        public IActionResult GetByAccountId(int accountId)
+        {
+            var result = _folderService.GetByAccountId(accountId);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Folder folder)
+        {
+            var result = _folderService.Add(folder);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Folder folder)
+        {
+            var result = _folderService.Update(folder);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
+
