417d6188b7639177922453dd824e45b36bc2234e
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-06-29 12:23:18 +0300

    Add the StudySet entity's Business objects

---
+++ Business/Abstract/IStudySetService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+	public interface IStudySetService
+	{
+		IResult Add(StudySet studySet);
+		IResult Update(StudySet studySet);
+		IDataResult<List<StudySet>> GetAll();
+		IDataResult<StudySet> GetById(int studySetId);
+        IDataResult<List<StudySet>> GetByAccountId(int accountId);
+        IDataResult<List<StudySet>> GetByFolderId(int folderId);
+		IDataResult<List<StudySet>> GetByNativeAndTargetLanguageIds(int[] nativeAndTargetLanguageIds);
+	}
+}
+
---
+++ Business/Concrete/StudySetManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Business.Abstract;
+using Business.Constants.Messages.Abstract;
+using Core.Utilities.Results.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+	public class StudySetManager : IStudySetService
+	{
+        IStudySetDal _studySetDal;
+        IMessage _message;
+
+		public StudySetManager(IStudySetDal studySetDal, IMessage message)
+		{
+            _studySetDal = studySetDal;
+            _message = message;
+		}
+
+        public IResult Add(StudySet studySet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<StudySet>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<StudySet>> GetByAccountId(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<StudySet>> GetByFolderId(int folderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<StudySet> GetById(int studySetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<StudySet>> GetByNativeAndTargetLanguageIds(int[] nativeAndTargetLanguageIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(StudySet studySet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
