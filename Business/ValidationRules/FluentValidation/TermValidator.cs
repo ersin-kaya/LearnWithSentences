2db89ab4da6ed5bdfc0588b5c5cae0882cff16a4
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-16 16:48:13 +0300

    Add the TermValidator.cs

---
+++ Business/ValidationRules/FluentValidation/TermValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+	public class TermValidator : AbstractValidator<Term>
+	{
+		public TermValidator()
+		{
+			RuleFor(t => t.TermText).NotEmpty();
+			RuleFor(t => t.TermText).MaximumLength(180);
+		}
+	}
+}
+
