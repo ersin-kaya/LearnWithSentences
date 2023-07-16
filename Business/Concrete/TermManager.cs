2e9a5f9a64918128b08c01e7ad4a868f10e282b2
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-16 18:33:55 +0300

    Added some aspects to all methods in TermManager.cs

--- Business/Concrete/TermManager.cs
+++ Business/Concrete/TermManager.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants.Messages.Abstract;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
@@ -19,12 +23,17 @@ namespace Business.Concrete
             _message = message;
 		}
 
+        [SecuredOperation("term.add,term,admin")]
+        [ValidationAspect(typeof(TermValidator))]
+        [CacheRemoveAspect("ITermService.Get")]
         public IResult Add(Term term)
         {
             _termDal.Add(term);
             return new SuccessResult(_message.TermAdded);
         }
 
+        [SecuredOperation("term.delete,term,admin")]
+        [CacheRemoveAspect("ITermService.Get")]
         public IResult Delete(Term term)
         {
             term.Visibility = false;
@@ -32,16 +41,21 @@ namespace Business.Concrete
             return new SuccessResult(_message.TermDeleted);
         }
 
+        [CacheAspect]
         public IDataResult<Term> GetById(int id)
         {
             return new SuccessDataResult<Term>(_termDal.Get(t => t.Id == id && t.Visibility == true));
         }
 
+        [CacheAspect]
         public IDataResult<List<Term>> GetByStudySetId(int studySetId)
         {
             return new SuccessDataResult<List<Term>>(_termDal.GetAll(t => t.StudySetId == studySetId && t.Visibility == true));
         }
 
+        [SecuredOperation("term.update,term,admin")]
+        [ValidationAspect(typeof(TermValidator))]
+        [CacheRemoveAspect("ITermService.Get")]
         public IResult Update(Term term)
         {
             _termDal.Update(term);
