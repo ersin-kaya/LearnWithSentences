2ca492742ca00c719543969c2e669b198c10bc99
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-18 11:44:36 +0300

    Add DefinitionValidator.cs

---
+++ Business/ValidationRules/FluentValidation/DefinitionValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+	public class DefinitionValidator : AbstractValidator<Definition>
+	{
+		public DefinitionValidator()
+		{
+			RuleFor(d => d.DefinitionText).NotEmpty().When(d=> d.ImagePath==null || d.ImagePath == "");
+			RuleFor(d => d.DefinitionText).MaximumLength(250);
+		}
+	}
+}
+
